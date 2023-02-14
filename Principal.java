bd205e549ed25cd935e10cdbad80a46e074c784a
OROMERO663 <oromeroe663@alumnos.imf.com>
2023-02-14 10:15:24 +0100

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,52 @@
+package crudArray;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		/*
+		 * Hacer el menu de opciones
+		 * 
+		 * 1 Crear
+		 * 2 consultar
+		 * 3 Modificar
+		 * 4 Eliminar - puede que se haga Modificando por hueco vacio
+		 * 5 Salir
+		 * 
+		 * Tema: Juegoteca
+		 * Juego y 4 máximo para probar
+		 * Ampliar a 20 juegos cuando funcione
+		 * 
+		 * Luego ampliar a Juego Categoria Jugadores
+		 *
+		 * 
+		 * Hacer la opcion Crear
+		 * Te tiene que pedir el nombre del juego y guardarlo en el primer
+		 * hueco libre del array
+		 * 
+		 * Luego intentar que te pida el Nombre Categ y Jugadores
+		 *
+		 * Hacer la opcion consultar
+		 * 
+		 * Te tiene que dejar ver si tienes un juego guardado (buscar por nombre)
+		 * 
+		 * Lo veo dificil pero podrían buscar tambien por categoria y jugadores??
+		 * 
+		 * Hacer la opcion modificar
+		 * 
+		 * Te deja modificar el nombre
+		 * 
+		 * Te deja modificar el nombre cat y jug
+		 * 
+		 * Hacer la opcion Eliminar
+		 * 
+		 * Lo mismo que modificar pero modifica a vacio ese hueco
+		 * se me ocurre un consultar primero y modificar a vacio después
+		 * 
+		 * Hacer la opcion Salir - finaliza el programa
+		 * 
+		 * 
+		 */
+
+	}
+
+}
