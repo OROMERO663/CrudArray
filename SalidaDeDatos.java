e89682c3209a102928d2ba492679c8bbd6d97e71
OROMERO663 <oromeroe663@alumnos.imf.com>
2023-02-16 11:16:56 +0100

    Actualizaciones día 4
    Cambios en método Crear para que avise cuando llega al límite
    Añadido método Eliminar
    Añadido método Lista en la clase SalidaDeDatos

--- SalidaDeDatos.java
+++ SalidaDeDatos.java
@@ -1,9 +1,11 @@
 package crudArray;
 
+import java.util.Arrays;
+
 public class SalidaDeDatos {
-	
+
 	public static void mostrarInicio(String texto1, String texto2, String texto3, String texto4) {
-		
+
 		System.out.println(texto1);
 		System.out.println("*************************");
 		System.out.println(texto2);
@@ -11,11 +13,11 @@ public class SalidaDeDatos {
 		System.out.println(texto3);
 		System.out.println("*************************");
 		System.out.println(texto4);
-		
 
 	}
+
 	public static void mostrarMenu() {
-		
+
 		System.out.println("¿Qué quieres hacer con tu lista de juegos?");
 		System.out.println("1 - Añadir");
 		System.out.println("2 - Consultar");
@@ -23,8 +25,12 @@ public class SalidaDeDatos {
 		System.out.println("4 - Eliminar");
 		System.out.println("5 - Salir");
 
-	
+	}
 
+	public static void TuLista(String[] juegos) {
+		System.out.println("Tu lista de juegos actual es:");
+		System.out.println(Arrays.toString(juegos));
+		System.out.println("*************************");
 	}
 
 }
